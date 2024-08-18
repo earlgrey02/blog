43aab86f99209fafd1ebfebf5e43aa2d024f9675
earlgrey02 <san06036@naver.com>
2024-08-19 02:22:38 +0900

    Refactor: ColorMode를 설정하는 부분을 ChakraProvider로 이동

--- src/component/RootProvider.tsx
+++ src/component/RootProvider.tsx
@@ -2,8 +2,6 @@ import VercelProvider from '@/lib/vercel/component/VercelProvider'
 import ReduxProvider from '@/lib/redux/component/ReduxProvider'
 import ChakraProvider from '@/lib/chakra/component/ChakraProvider'
 import type { ReactNode } from 'react'
-import { ColorModeScript } from '@chakra-ui/react'
-import config from '@/lib/chakra/config'
 
 interface Props {
   children: ReactNode
@@ -13,10 +11,7 @@ const RootProvider = ({ children }: Props) => {
   return (
     <VercelProvider>
       <ReduxProvider>
-        <ChakraProvider>
-          <ColorModeScript initialColorMode={config.initialColorMode} />
-          {children}
-        </ChakraProvider>
+        <ChakraProvider>{children}</ChakraProvider>
       </ReduxProvider>
     </VercelProvider>
   )
--- src/lib/chakra/component/ChakraProvider.tsx
+++ src/lib/chakra/component/ChakraProvider.tsx
@@ -1,14 +1,20 @@
 'use client'
-import { ChakraProvider as ChakraOriginProvider } from '@chakra-ui/react'
+import { ChakraProvider as ChakraOriginProvider, ColorModeScript } from '@chakra-ui/react'
 import type { ReactNode } from 'react'
 import theme from '@/lib/chakra/theme'
+import config from '@/lib/chakra/config'
 
 interface Props {
   children: ReactNode
 }
 
 const ChakraProvider = ({ children }: Props) => {
-  return <ChakraOriginProvider theme={theme}>{children}</ChakraOriginProvider>
+  return (
+    <ChakraOriginProvider theme={theme}>
+      <ColorModeScript initialColorMode={config.initialColorMode} />
+      {children}
+    </ChakraOriginProvider>
+  )
 }
 
 export default ChakraProvider
