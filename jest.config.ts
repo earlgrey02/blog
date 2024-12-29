d0e87e65398ca125537efdd77080985f651010fe
earlgrey02 <san06036@naver.com>
2024-12-30 07:01:22 +0900

    Fix: Jest에서 경로 alias를 해석하지 못하던 문제 해결

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,10 @@ const createJestConfig = nextJest({
 
 const config: Config = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 }
 
 export default createJestConfig(config)
