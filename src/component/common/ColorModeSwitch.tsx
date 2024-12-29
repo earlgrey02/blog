273b36fc1803a0eefeba01d16e158b1a62206509
earlgrey02 <san06036@naver.com>
2024-12-30 05:10:14 +0900

    Refactor: ColorModeSwitch에서 Button 컴포넌트에 정의된 스타일과 중복되는 부분 삭제

--- src/component/common/ColorModeSwitch.tsx
+++ src/component/common/ColorModeSwitch.tsx
@@ -1,23 +1,20 @@
 'use client'
 
-import { Moon, Sun } from 'lucide-react'
+import { Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import type { ButtonProps } from '@/component/ui/Button'
 import Button from '@/component/ui/Button'
 import { ColorMode } from '@/lib/shadcn-ui/enum'
-import { cn } from '@/lib/shadcn-ui/util'
 
 type Props = ButtonProps
 
-const ColorModeSwitch = ({ className, ...props }: Props) => {
+const ColorModeSwitch = ({ ...props }: Props) => {
   const { resolvedTheme: theme, setTheme } = useTheme()
 
   return (
     <Button
       variant="ghost"
       size="icon"
-      slot="0.5rem"
-      className={cn('rounded-full', className)}
       onClick={() => setTheme(theme === ColorMode.LIGHT ? ColorMode.DARK : ColorMode.LIGHT)}
       {...props}>
       {theme === ColorMode.LIGHT ? <Moon /> : <Sun />}
