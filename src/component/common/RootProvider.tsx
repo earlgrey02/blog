83366aa43727e8296b83d99e00440f069143a16e
earlgrey02 <san06036@naver.com>
2025-01-08 05:38:06 +0900

    Feat: VercelProvider 구현 및 RootProvider에 적용

--- src/component/common/RootProvider.tsx
+++ src/component/common/RootProvider.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import type { ReactNode } from 'react'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { ThemeProvider } from 'next-themes'
 import { Provider as ReduxProvider } from 'react-redux'
 import store from '@/lib/redux/store'
@@ -9,13 +11,25 @@ interface Props {
   children: ReactNode
 }
 
+const VercelProvider = ({ children }: Props) => {
+  return (
+    <>
+      <Analytics />
+      <SpeedInsights />
+      {children}
+    </>
+  )
+}
+
 const RootProvider = ({ children }: Props) => {
   return (
-    <ReduxProvider store={store}>
-      <ThemeProvider attribute="class" disableTransitionOnChange>
-        {children}
-      </ThemeProvider>
-    </ReduxProvider>
+    <VercelProvider>
+      <ReduxProvider store={store}>
+        <ThemeProvider attribute="class" disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </ReduxProvider>
+    </VercelProvider>
   )
 }
 
