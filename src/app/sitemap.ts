d30d1cbec622e72efc297687ee01e38f31f0f5aa
earlgrey02 <san06036@naver.com>
2025-01-08 05:53:13 +0900

    Feat: sitemap.xml 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,28 @@
+import { getPosts } from '@/lib/contentlayer/util'
+import type { Sitemap } from '@/lib/type/seo'
+
+const sitemap = (): Sitemap => {
+  const sitemaps: Sitemap = getPosts().map(post => ({
+    url: `${process.env.NEXT_URL}/post/${post.id}`,
+    lastModified: new Date(post.date),
+    changeFrequency: 'weekly'
+  }))
+
+  return [
+    {
+      url: process.env.NEXT_URL,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8
+    },
+    {
+      url: `${process.env.NEXT_URL}/post`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1
+    },
+    ...sitemaps
+  ]
+}
+
+export default sitemap
