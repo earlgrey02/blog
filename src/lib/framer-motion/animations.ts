9f6a4d6d2704dcca4e22a4c870d2e00d2e4df000
earlgrey02 <san06036@naver.com>
2025-01-07 22:14:25 +0900

    Feat: 애니메이션 프리셋 구현

---
+++ src/lib/framer-motion/animations.ts
@@ -0,0 +1,20 @@
+'use client'
+import type { Variants } from 'framer-motion'
+
+const fadeIn: Variants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 }
+}
+
+const fadeInRight: Variants = {
+  initial: {
+    opacity: 0,
+    x: -10
+  },
+  animate: {
+    opacity: 1,
+    x: 0
+  }
+}
+
+export { fadeIn, fadeInRight }
