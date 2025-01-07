b07a4c04afb001aa0c41d507c758d386d9f839e5
earlgrey02 <san06036@naver.com>
2025-01-08 03:56:52 +0900

    Feat: 게시글 전체 조회 유틸 추가

--- src/lib/contentlayer/util.ts
+++ src/lib/contentlayer/util.ts
@@ -1,8 +1,10 @@
-import { allPosts as posts } from 'contentlayer'
+import { allPosts, allPosts as posts } from 'contentlayer'
 
 const getPostById = (id: string) => posts.find(post => post.id === id)
+
+const getPosts = () => allPosts
 
 const getPostsOrderByDate = () =>
   posts.sort((post1, post2) => new Date(post2.date).getTime() - new Date(post1.date).getTime())
 
-export { getPostById, getPostsOrderByDate }
+export { getPostById, getPosts, getPostsOrderByDate }
