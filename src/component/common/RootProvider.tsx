a62d3fe40d2de8461560612f2236d4d4546c2a4b
earlgrey02 <san06036@naver.com>
2025-01-07 22:30:28 +0900

    Feat: RootProvider에 ReduxProvider 추가

--- src/component/common/RootProvider.tsx
+++ src/component/common/RootProvider.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import type { ReactNode } from 'react'
 import { ThemeProvider } from 'next-themes'
+import { Provider as ReduxProvider } from 'react-redux'
+import store from '@/lib/redux/store'
 
 interface Props {
   children: ReactNode
@@ -7,9 +11,11 @@ interface Props {
 
 const RootProvider = ({ children }: Props) => {
   return (
-    <ThemeProvider attribute="class" disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </ReduxProvider>
   )
 }
 
