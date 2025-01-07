8d939f30ff90ce055dbac43876dcf9b674a26d5e
earlgrey02 <san06036@naver.com>
2025-01-07 19:18:51 +0900

    Feat: TSLint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,19 @@
-const config = []
+import tslint from 'typescript-eslint'
+
+const config = tslint.config(
+  tslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-exports': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error'
+    }
+  }
+)
 
 export default config
