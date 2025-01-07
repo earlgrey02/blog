143123d67e5294757e8896881bac8e47be03fbd1
earlgrey02 <san06036@naver.com>
2025-01-07 19:21:56 +0900

    Feat: ESLint import 플러그인 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import tslint from 'typescript-eslint'
 import tailwindPlugin from 'eslint-plugin-tailwindcss'
+import importPlugin from 'eslint-plugin-import'
 
 const config = tslint.config(
   tslint.configs.recommended,
@@ -14,6 +15,31 @@ const config = tslint.config(
     rules: {
       '@typescript-eslint/consistent-type-exports': 'error',
       '@typescript-eslint/consistent-type-imports': 'error'
+    }
+  },
+  {
+    plugins: { import: importPlugin },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'after'
+            },
+            {
+              pattern: 'next',
+              group: 'builtin',
+              position: 'after'
+            }
+          ],
+          pathGroupsExcludedImportTypes: ['react', 'next'],
+          alphabetize: { order: 'asc' }
+        }
+      ]
     }
   }
 )
