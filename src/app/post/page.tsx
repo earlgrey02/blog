80e02979efb49043b56b91618ba1fad2bfab2229
earlgrey02 <san06036@naver.com>
2025-01-08 02:36:41 +0900

    Feat: 게시글 리스트 페이지 구현

---
+++ src/app/post/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useState } from 'react'
+import type { Post } from 'contentlayer'
+import { allPosts } from 'contentlayer'
+import { useSelector } from 'react-redux'
+import PostList from '@/component/post/PostList'
+import PostPaginator from '@/component/post/PostPaginator'
+import Motion from '@/component/ui/Motion'
+import { fadeIn } from '@/lib/framer-motion/animations'
+
+const Page = () => {
+  const { pageIndex } = useSelector(store => store.post)
+  const [delayChildren, setDelayChildren] = useState(0.5)
+  const pages = allPosts.reduce<Post[][]>((pages, post, index) => {
+    const pageIndex = Math.floor(index / 4)
+
+    if (!pages[pageIndex]) pages[pageIndex] = []
+    pages[pageIndex].push(post)
+
+    return pages
+  }, [])
+
+  return (
+    <Motion
+      className="flex flex-col gap-9"
+      initial="initial"
+      animate="animate"
+      transition={{
+        staggerChildren: 0.8
+      }}>
+      <Motion className="text-[1.3rem] font-extrabold md:text-[1.7rem]" variants={fadeIn} transition={{ duration: 1 }}>
+        게시글
+      </Motion>
+      <PostList
+        posts={pages[pageIndex]}
+        key={pageIndex}
+        transition={{
+          staggerChildren: 0.2,
+          delayChildren
+        }}
+      />
+      <Motion variants={fadeIn} transition={{ duration: 1 }} onClick={() => setDelayChildren(0)}>
+        <PostPaginator totalPage={pages.length} />
+      </Motion>
+    </Motion>
+  )
+}
+
+export default Page
