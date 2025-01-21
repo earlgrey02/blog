70d825dbf10ffb2c79a1c31055fbcdf8f25daec7
earlgrey02 <san06036@naver.com>
2025-01-22 03:00:29 +0900

    Feat: PostStates에 filter 추가

--- src/lib/redux/reducer/postReducer.ts
+++ src/lib/redux/reducer/postReducer.ts
@@ -3,18 +3,30 @@ import type { PayloadAction } from '@reduxjs/toolkit'
 
 interface PostState {
   pageIndex: number
+  filter: Filter
+}
+
+interface Filter {
+  tag?: string
 }
 
 const { reducer, actions } = createSlice({
   name: 'post',
-  initialState: { pageIndex: 0 } satisfies PostState,
+  initialState: {
+    pageIndex: 0,
+    filter: {}
+  } as PostState,
   reducers: {
     setPage: (state: PostState, action: PayloadAction<number>) => ({
       ...state,
       pageIndex: action.payload
+    }),
+    setFilter: (state: PostState, action: PayloadAction<Filter>) => ({
+      ...state,
+      filter: action.payload
     })
   }
 })
 
-export const { setPage } = actions
+export const { setPage, setFilter } = actions
 export default reducer
